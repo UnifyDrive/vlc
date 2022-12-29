b30b0447dae9e3334b0b0859e71060336a6a5030
shixuehui <shixuehui@zspace.cn>
2022-12-29 20:57:55 +0800

    [Bugfix/ALL] Try 3 times while http read failed.

--- modules/access/http/file.c
+++ modules/access/http/file.c
@@ -289,19 +289,24 @@ block_t *vlc_http_file_read(struct vlc_http_resource *res)
 {
     struct vlc_http_file *file = (struct vlc_http_file *)res;
     block_t *block = vlc_http_res_read(res);
+    int try_times = 3;
 
-    if (block == vlc_http_error)
+    while (block == vlc_http_error)
     {   /* Automatically reconnect on error if server supports seek */
-        if (res->response != NULL
+        vlc_http_res_destroy_response_bak(res);
+        if (/*res->response != NULL
          && vlc_http_msg_can_seek(res->response)
          && file->offset < vlc_http_msg_get_file_size(res->response)
-         && vlc_http_file_seek(res, file->offset) == 0)
+         && */vlc_http_file_seek(res, file->offset) == 0)
             block = vlc_http_res_read(res);
 
-        if (block == vlc_http_error)
-            return block;
-    }
+        if(try_times-- <= 0) {
+            break;
+        }
+    };
 
+    if (block == vlc_http_error)
+        return block;
     if (block == NULL)
         return NULL; /* End of stream */
 
