9c38613d8bd70163e6fbe304ca04be0f7a60ea37
shixuehui <shixuehui@zspace.cn>
2024-04-08 14:44:17 +0800

    [Bugfix/ALL] Can not get duration for some ts.

--- modules/demux/avformat/demux.c
+++ modules/demux/avformat/demux.c
@@ -378,7 +378,7 @@ int avformat_OpenDemux( vlc_object_t *p_this )
     vlc_avcodec_lock(); /* avformat calls avcodec behind our back!!! */
     // TDX  ...
     //error = avformat_find_stream_info( p_sys->ic, options );
-    if(strcmp( fmt->name, "flv" ) != 0 && strcmp( fmt->name, "wav" ) != 0 && !var_InheritBool( p_demux, "spdif" )){
+    if(strcmp( fmt->name, "flv" ) != 0 && strcmp( fmt->name, "wav" ) != 0 && !var_InheritBool( p_demux, "spdif" ) && strcmp( p_sys->fmt->name, "mpegts" ) != 0){
         av_opt_set_int(p_sys->ic, "analyzeduration", 500000, 0);
         av_opt_set_int(p_sys->ic, "probesize", 4096, 0);
     }
